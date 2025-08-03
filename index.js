40bc0764293c81f2e9cd59f77fb9165b90da9295
Jonathan Luzuriaga <118029879+LuzuJ@users.noreply.github.com>
2025-08-02 21:34:26 -0500

    Build, Lint, Test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 // Componentes para generar nombres y características
 const prefijos = ["Xy", "Zor", "Glar", "Kry", "Ve"];
